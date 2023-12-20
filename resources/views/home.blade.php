d3075874649428e058acfccff2aa3356df0b5a53
unknown <f.carrara24@studenti.unibg.it>
2023-12-20 17:09:50 +0100

    changed homepage view style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,20 @@
 
 @section('content')
     <main>
-        <h1>Home page</h1>
+        <h1 class="text-center">Home page</h1>
+        <div class="d-flex flex-row justify-content-center gap-3 pt-5 align-items-center">
+
+        <div class="myCircle d-flex flex-row justify-content-center  align-items-center ">
+            <a href="{{ route('movies.index') }}">films</a>
+        </div>
+        <div class="myCircle d-flex flex-row justify-content-center  align-items-center ">
+            <a href="{{ route('books.index') }}">books</a>
+        </div>
+
+
+    </div>
     </main>
+
 
 
 
